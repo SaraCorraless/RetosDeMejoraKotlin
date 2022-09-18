90050a146435f8320c1603dcc7bc8351b2c82d3c
Sara <saracorraless06@gmail.com>
2022-09-18 16:50:38 +0200

    Nuevos retos 6, 7 y 8

--- src/main/kotlin/Reto8.kt
+++ src/main/kotlin/Reto8.kt
@@ -5,6 +5,7 @@
  * Fecha publicación resolución: 21/02/22
  * Dificultad: MEDIA
  *
+ *
  * Enunciado: Crea un programa que cuente cuantas veces se repite cada palabra y que muestre el recuento final de todas ellas.
  * - Los signos de puntuación no forman parte de la palabra.
  * - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
