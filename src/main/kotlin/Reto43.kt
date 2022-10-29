1c41152c9f5ce643d069fead4db78349a4d7513e
Sara <71656774+SaraCorraless@users.noreply.github.com>
2022-10-29 19:16:31 +0200

    Reto 43

---
+++ src/main/kotlin/Reto43.kt
@@ -0,0 +1,18 @@
+/*
+ * Enunciado: Crea una función que transforme grados Celsius en Fahrenheit
+ * y viceversa.
+ *
+ * - Para que un dato de entrada sea correcto debe poseer un símbolo "°"
+ *   y su unidad ("C" o "F").
+ * - En caso contrario retornará un error.
+ * - ¿Quieres emplear lo aprendido en este reto?
+ *   Revisa el reto mensual y crea una App:
+ *   https://retosdeprogramacion.com/mensuales2022
+ */
+
+class Reto43() {
+
+    fun gradosFahrenheit(){
+
+    }
+}
